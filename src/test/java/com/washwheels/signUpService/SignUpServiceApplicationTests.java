6818ab82436da6f1e1191c51bfea415881138d53
Raghava <1raghuram3@gmail.com>
2020-06-14 12:30:22 +0530

    skeleton of the signup microservice created.

---
+++ src/test/java/com/washwheels/signUpService/SignUpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washwheels.signUpService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignUpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
